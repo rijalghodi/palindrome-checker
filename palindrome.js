9bddb6e9bb5e58acdc5d45c0502278cd743236c1
rizal ghod <rijalghodi10@gmail.com>
2022-08-26 11:05:22 +0700

    edit comments

--- palindrome.js
+++ palindrome.js
@@ -1,46 +1,74 @@
+/*
+Created by Rijal Ghodi on August 25, 2022
+email : rijalghodi10@gmail.com
+github : https://github.com/rijalghodi
+
+This is a javascript code to check if a string is a palindrome
+
+Palindrome is a word or sentence that's spelled the same way both forward and 
+backward, ignoring punctuation, case, and spacing.
+
+*/
+
+// + Define palindrome function, accepting string as an argument
 function palindrome(str) {
-  // upper the str to simplify the problem
-  let check = str.toUpperCase();
+  //
+  // To simplify the problem, upper the string
+  let upperStr = str.toUpperCase();
   let i = 0;
   let j = str.length - 1;
+
+  // + Check if every leftmost and righmost char is same
   while (true) {
-    // extract the ASCII value of char
-    let leftValue = check.charCodeAt(i);
-    let rightValue = check.charCodeAt(j);
+    //
+    // + extract the ASCII value of leftmost and rightmost char
+    let leftChar = upperStr.charCodeAt(i);
+    let rightChar = upperStr.charCodeAt(j);
+
+    //
     // Make sure that there is at least 2 char to be checked
     if (j - i > 0) {
-      // check if leftValue is alphanumeric
-      if ((leftValue >= 65 && leftValue <= 90) || (leftValue >= 48 && leftValue <= 57)) {
-        // check if rightValue is alphanumeric
-        if ((rightValue >= 65 && rightValue <= 90) || (rightValue >= 48 && rightValue <= 57)) {
-          // check if leftValue equals to rightValue
-          if (leftValue == rightValue) {
+      //
+      // Make sure that leftmost Char is alphanumeric
+      if ((leftChar >= 65 && leftChar <= 90) || (leftChar >= 48 && leftChar <= 57)) {
+        //
+        // Make sure that rightmost Char is also alphanumeric
+        if ((rightChar >= 65 && rightChar <= 90) || (rightChar >= 48 && rightChar <= 57)) {
+          //
+          // Make sure that leftmost char is same as rightmost char
+          if (leftChar == rightChar) {
+            // if so, continue checking the next leftmost and rightmost char
             i += 1;
             j -= 1;
             continue;
           }
-          // if the leftValue inequal to rightValue
+          // if the leftmost char is not same as rightmost char, return false
           else {
             return false;
           }
         }
-        // if the rightValue is not an alphanumeric
-        // shift the rightValue 1 char to left
+        // if the rightmost char is not alphanumeric, ignore it and
+        // shift to left by one-position
         else {
           j -= 1;
           continue;
         }
       }
-      // if the leftValue is not analphanumeric
-      // shift the leftValue 1 char to right
+      // if the leftmost char is not analphanumeric, ignore it and
+      // shift to right by one-position
       else {
         i += 1;
         continue;
       }
-    } else {
+    }
+    // if there is one or zero char in string, or if all of the leftmost
+    // alphanumeric char is same as rightmost alphanumeric char, then
+    // return true
+    else {
       return true;
     }
   }
 }
 
+// + Test
 console.log(palindrome("eye"));
